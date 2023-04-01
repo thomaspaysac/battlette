d0aeb86f70e5779d21b35228eb73b4cc81472684
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-04-01 17:30:10 +0200

    Update player.test.js

--- src/player.test.js
+++ src/player.test.js
@@ -23,6 +23,12 @@ it('should be able to place ships on a player board', () => {
   expect(playerBoard).toEqual(testBoard.board);
 });
 
+it('should access player\'s ships', () => {
+  let player1 = Player('Player 1');
+  player1.board.placeShip(2, [0,0], 'Carrier');
+  expect(JSON.stringify(player1.board.shipList)).toBe(`[{\"name\":\"Carrier\",\"size\":2,\"hits\":0}]`);
+});
+
 it('should attack the other player\'s board', () => {
   let player1 = Player('Player1');
   player1.board.placeShip(2, [0,0], 'Carrier');
@@ -32,7 +38,15 @@ it('should attack the other player\'s board', () => {
   expect(testBoard[0][0]).toBe('X');
 });
 
-it.only('should confirm hit', () => {
+it('should change player\'ship hit property when attacked', () => {
+  let player1 = Player('Player 1');
+  player1.board.placeShip(2, [0,0], 'Carrier');
+  let player2 = Player('Player2');
+  player2.attack(player1, 0, 0);
+  expect(player1.board.shipList[0].hits).toBe(1);
+});
+
+it('should confirm hit', () => {
   let player1 = Player('Player1');
   player1.board.placeShip(2, [0,0], 'Carrier');
   let testBoard = player1.board.board;
@@ -46,5 +60,5 @@ it('should send an error when attacking twice on the same spot', () => {
   let testBoard = player1.board.board;
   let player2 = Player('Player2');
   player2.attack(player1, 0, 0);
-  expect(player2.attack(player1, 0, 0)).toThrow();
+  expect(() => player2.attack(player1, 0, 0)).toThrow();
 });
